9cc48d46a2fb24abbc425cb7f278122b09a5484f
Akooshsoohoo <1sahakash1@gmail.com>
2025-02-22 21:27:40 -0500

    Added button functionality to popup.js

---
+++ popup/popup.js
@@ -0,0 +1,15 @@
+let buttons = document.querySelectorAll("button"); // select all the buttons
+
+buttons[0].addEventListener("click", function() {
+    alert("Correct!");
+});
+
+buttons[1].addEventListener("click", function() {
+    alert("Inccorrect.");
+});
+buttons[2].addEventListener("click", function() {
+    alert("Inccorrect.");
+});
+buttons[3].addEventListener("click", function() {
+    alert("Inccorrect.");
+});
